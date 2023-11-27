0dd55e9dad65b228f0740636ac50db7d6e1f04a1
Fernando <73785564+fernandopassoss@users.noreply.github.com>
2023-11-27 15:54:04 -0300

    Add files via upload

--- quicktalk-php/login.php
+++ quicktalk-php/login.php
@@ -1,3 +1,10 @@
+<?php 
+    session_start();
+    if(isset($_SESSION['unique_id'])){
+        header('location: users.php');
+    }
+?>
+
 <?php include_once "head.php" ?>
 <body>
 <video class="video-background" autoplay muted loop>
